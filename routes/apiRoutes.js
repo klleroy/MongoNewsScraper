1b7f81c8bf92500067d79a2f448893f2f2bee8d5
KevinL <mastahkevin@gmail.com>
2019-06-30 23:17:17 -0700

    continued buildout of api routes. Added in NY Times Sports section for axios getter.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,19 @@
 const axios = require ('axios');
 const cheerio = require ('cheerio');
 const db = require ('../models');
+
+module.exports = function (app) {
+    app.get("/scrape", (req, res) => {
+        for (let i = 0; i < 1; i++) {
+            axios.get('https://www.nytimes.com/section/sports' + i).then(response => {
+                const $ = cheerio.load(response.data);
+
+                let result = {};
+
+                $('a.articleTitle').each((i, element) => {
+                    const title = $(element).find('h3').find()
+                })
+            })
+        }
+    })
+}
