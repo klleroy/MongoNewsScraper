8c9cd100eded387478975de9a81d2628f401f7cc
KevinL <mastahkevin@gmail.com>
2019-06-30 23:36:24 -0700

    Adding in basic building blocks for scrape route.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,18 +2,18 @@ const axios = require ('axios');
 const cheerio = require ('cheerio');
 const db = require ('../models');
 
-module.exports = function (app) {
-    app.get("/scrape", (req, res) => {
-        for (let i = 0; i < 1; i++) {
-            axios.get('https://www.nytimes.com/section/sports' + i).then(response => {
-                const $ = cheerio.load(response.data);
 
-                let result = {};
-
-                $('a.articleTitle').each((i, element) => {
-                    const title = $(element).find('h3').find()
-                })
+app.get("/scrape", (req, res) => {
+    for (let i = 0; i < 1; i++) {
+        axios.get('https://www.nytimes.com/section/sports' + i).then(response => {
+            const $ = cheerio.load(response.data);
+            // Save an empty result object
+            let result = {};
+            
+            // Add the text and href of every link, and save them as properties of the result object
+            $('a.articleTitle').each((i, element) => {
+                const title = $(element).find('h3').find()
             })
-        }
-    })
-}
+        })
+    }
+})
