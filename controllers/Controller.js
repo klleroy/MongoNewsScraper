453dbcfd70ca4ef43a78577642bd1d058be9a60b
KevinL <mastahkevin@gmail.com>
2019-07-07 09:40:23 -0700

    Refining Controller to be much more lean.

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -14,27 +14,72 @@ const Comment = require('../models/Comment');
 router.get('/',(req, res) => {
      res.redirect('/articles');
 });
+/* scraping router */
+// router.get('/scrape',(req, res) => {
+//      request('https://www.mlb.com/news', (error, response) => {
+//           const $ = cheerio.load(html);
+//           const titleArray = [];
 
-router.get('/scrape',(req, res) => {
-     request('https://www.mlb.com/news', (error, response) => {
-          const $ = cheerio.load(html);
-          const titleArray = [];
+//           $('.article-item__headline').each((i, element) => {
+//                let result = {};
 
-          $('.article-item__headline').each((i, element) => {
-               let result = {};
-
-               result.title = $(this)
-               .children('a')
-               .text();
+//                result.title = $(this)
+//                .children('a')
+//                .text();
                
-               result.link = $(this)
-               .children('a')
-               .attr('href');
+//                result.link = $(this)
+//                .children('a')
+//                .attr('href');
 
-               if(result.title !== '' && result.link !== ''){
+//                if(result.title !== '' && result.link !== ''){
+//                     if (titleArray.indexOf(result.title) === -1) {
+//                          titleArray.push(result.title);
+
+//                          Article.count({ title: result.title }, (err, test) => {
+//                               if(test === 0) {
+//                                    let entry = new Article(result);
+
+//                                    entry.save((err, doc) => {
+                                        
+//                                    })
+//                               }
+//                          })
+//                     } else {
+//                          console.log('Article already exists');
+//                     }
+//                } else {
+//                     console.log('Not saved to DB. Missing Data.');
                     
-               }
-          })
+//                }
+//           })
+//      })
+// });
+
+router.get("/scrape", (req, res) => {
+     axios.get("https://mlb.com/news/").then((response) => {
+ 
+         let $ = cheerio.load(response.data);
+ 
+         //save an empty result object 
+         let result = {};
+ 
+         $("article.article-item--article").each((i, element) => {
+             
+                 //add the title and excerpt of every article, and save them as properties of the result
+                 result.title = $(this).find("h1.article-item__headline").text();
+                 result.excerpt = $(this).find("article-item__preview.p").text();
+                 result.link = $(this).find("div").attr("onclick").replace(/location.href='/, "").slice(0, -1);
+             
+                     Article.create(result)
+                     .then((dbArticle) => {
+                         //view the added result in the console
+                         console.log(dbArticle);
+                     })
+                     .catch(function (err) {
+                         console.log(err);
+                     });
+         });
+         res.redirect('/');
      })
-});
+ });
 
